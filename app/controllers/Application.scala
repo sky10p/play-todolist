5dafd2e735f8af4dbbfc30a5b80d1152593eb760
Pablo Guijarro Ramirez <sky10pg@gmail.com>
2014-09-12 00:09:31 +0200

    Añadido el formulario del task

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,6 +2,12 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import play.api.data._
+import play.api.data.Forms._
+
+val taskForm = Form(
+  "label" -> nonEmptyText
+)
 
 object Application extends Controller {
 
